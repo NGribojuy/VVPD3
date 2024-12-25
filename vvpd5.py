994ab737aa912760a14ec8ad004021cdad873d04
Neo-Necron-26 <ruslanjoraeff@yandex.ru>
2024-12-26 01:03:53 +0700

    Добавлена новая функция

--- vvpd5.py
+++ vvpd5.py
@@ -43,7 +43,28 @@ def maclaurin_exp_minus_one(x, iterations=10):
         result += term
     return result
 
+def maclaurin_sqrt_one_minus_x(x, iterations=10):
+    """
+    Вычисляет sqrt(1 - x) с использованием ряда Маклорена.
 
+    Формула:
+    sqrt(1 - x) = 1 - x/2 - (x^2)/(8) - (x^3)/(16) + ...
+
+    Параметры:
+        x (float): значение x, для которого вычисляется sqrt(1 - x)
+        iterations (int): количество итераций ряда
+
+    Возвращает:
+        float: приближённое значение sqrt(1 - x)
+    """
+    if x <= -1 or x > 1:
+        raise ValueError("x должен быть в пределах (-1, 1] для данной формулы.")
+
+    result = 0
+    for m in range(iterations):
+        term = ((-1)**m) * (math.prod(range(1, 2 * m, 2)) / (2 * m * math.factorial(m))) * (x ** m)
+        result += term
+    return result
 
 def user_menu():
     """
@@ -53,6 +74,7 @@ def user_menu():
         print("\nМеню:")
         print("1. Вычислить cos(x) с использованием ряда Маклорена")
         print("2. Вычислить e^x - 1 с использованием ряда Маклорена")
+        print("3. Вычислить sqrt(1 - x) с использованием ряда Маклорена")
         print("4. Выход")
 
         choice = input("Выберите опцию (1/2/3/4): ")
@@ -72,8 +94,13 @@ def user_menu():
                 print(f"Приближённое значение e^{x} - 1 = {result}")
             except ValueError as e:
                 print(f"Ошибка: {e}")
-
- 
+        elif choice == '3':
+            try:
+                x = float(input("Введите значение x (должно быть в пределах (-1, 1]): "))
+                result = maclaurin_sqrt_one_minus_x(x)
+                print(f"Приближённое значение sqrt(1 - {x}) = {result}")
+            except ValueError as e:
+                print(f"Ошибка: {e}")
         elif choice == '4':
             print("Выход из программы.")
             break
